43c98973505539e8213f57d447f21b5f7ec8bc2b
Armin Hadziahmetovic <hadziahmetovic@bio.ifi.lmu.de>
2021-06-07 14:02:49 +0200

    fix on suppa input params

--- scripts/das_suppa2.sh
+++ scripts/das_suppa2.sh
@@ -109,7 +109,7 @@ for d in "READS" "STAR"; do
 
 	( [ -f "$out/diff_splicing_outs/SUPPA_salmon_$d.out" ] && echo "[INFO] [SUPPA2] $out/diff_splicing_outs/SUPPA_salmon_$d.out already exists, skipping.."$'\n' ) \
 			|| ($SUPPA diffSplice -m empirical --input $out/SUPPA2/$GTFNAME.ioi --psi $out/SUPPA2/$d*_isoform.psi \
-    	-e $out/SUPPA2/salmon_$d* -gc -o $out/SUPPA2/SUPPA_salmon_$d.out)
+    	-e $out/SUPPA2/salmon_$d.*.tmp.counts -gc -o $out/SUPPA2/SUPPA_salmon_$d.out)
 
 	kill -15 $wid
 
